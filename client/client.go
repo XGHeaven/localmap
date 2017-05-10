2a03039e724d9bac2ebd6b0cc17d590dc3169c9b
XGHeaven <xgheaven@gmail.com>
2017-05-10 18:35:50 +0800

    chore: remove unused code

--- client/client.go
+++ client/client.go
@@ -1,12 +1,10 @@
 package client
 
 import (
-	"fmt"
 	"io"
 	"net"
 	"syscall"
 
-	"github.com/xgheaven/localmap/conn"
 	"github.com/xgheaven/localmap/connect"
 	"github.com/xgheaven/localmap/logger"
 	"github.com/xgheaven/localmap/util"
@@ -116,79 +114,4 @@ func (client *Client) WaitEnd() {
 	<-util.NewInterruptChan(syscall.SIGTERM)
 	logger.Debug("receive SIGTERM sign")
 	client.WriteClose()
-}
-
-func Start() {
-	var (
-		sPort, cPort int
-	)
-	cConn, err := net.DialTCP("tcp", nil, &Option.SAddr)
-	if err != nil {
-		logger.Fatal("connect server error")
-	}
-
-	defer cConn.Close()
-
-	connect := conn.NewConnect(cConn)
-	connect.WriteString("hello\n")
-	connect.Flush()
-	data, err := connect.ReadWholeLine()
-	if err != nil {
-		return
-	}
-	fmt.Sscanf(string(data), "hello %d %d", &sPort, &cPort)
-	sAddr := Option.SAddr
-	cAddr := Option.SAddr
-	sAddr.Port = sPort
-	cAddr.Port = cPort
-	logger.Infof("connect to server, please use %s\n", sAddr.String())
-
-	go func() {
-		<-util.NewInterruptChan(syscall.SIGTERM)
-		connect.WriteString("bye\n")
-		connect.Flush()
-		logger.Info("waiting for server close port")
-		data, err := connect.ReadWholeLine()
-		if string(data) != "bye" || err != nil {
-			logger.Error("disconnect server port error")
-		} else {
-			logger.Info("server close port success")
-		}
-		logger.Info("client close")
-		cConn.Close()
-	}()
-
-	for {
-		data, err := connect.ReadWholeLine()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			continue
-		}
-		if string(data) == "request" {
-			go func() {
-				sConn, sErr := net.DialTCP("tcp", nil, &cAddr)
-
-				if sErr != nil {
-					logger.Error("connect to server error")
-					return
-				}
-
-				logger.Debug("connect to server")
-
-				cConn, cErr := net.DialTCP("tcp", nil, &Option.CAddr)
-
-				if cErr != nil {
-					logger.Error("connect to client error")
-					return
-				}
-
-				logger.Debug("connect to client")
-
-				go util.LinkConnect(cConn, sConn)
-				go util.LinkConnect(sConn, cConn)
-			}()
-		}
-	}
 }
--- server/server.go
+++ server/server.go
@@ -2,8 +2,6 @@ package server
 
 import (
 	"net"
-
-	"github.com/xgheaven/localmap/logger"
 )
 
 var Option ServerOption
@@ -21,24 +19,4 @@ func NewServer(option *ServerOption) *Server {
 		clients:      make(map[string]*ServerClient),
 	}
 	return server
-}
-
-func Start() {
-	listener, err := net.ListenTCP("tcp", &Option.Addr)
-
-	if err != nil {
-		logger.Fatal(err)
-	}
-
-	logger.Info("listen at " + Option.Addr.String())
-
-	for {
-		c, err := listener.AcceptTCP()
-		if err != nil {
-			logger.Error(err)
-			continue
-		}
-		logger.Debug("accept new client")
-		go HandleConnect(c)
-	}
 }
